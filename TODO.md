f2ac4f0a118fde51ac02e069d87165474c814f89
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-02-07 16:07:20 +0530

    Frontend created

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO: Style Update and Delete Buttons
+
+- [x] Add className="update-btn" to the update image in App.jsx
+- [x] Add className="delete-btn" to the delete image in App.jsx
+- [x] Update .btn-container in App.css to display as flex with gap
+- [x] Add styles for .update-btn and .delete-btn in App.css (size, cursor, hover effects)
---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import "./App.css"
+import axios from 'axios';
+import DeleteImg from "./assets/delete (1).png"
+import UpdateImg from "./assets/pen.png"
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [oldTodo, setOldTodo] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const Base_URL = "http://localhost:8008/todos";
+
+  const loadTodos = async () => {
+    console.log("Loading todos...");
+
+      const response = await axios.get(`${Base_URL}/todos`);
+      console.log(response.data.data);
+      setTodos(response.data.data);
+
+  };
+  const deleteTodo = async (todo) => {
+    const response = await axios.delete(`${Base_URL}/todos`,
+      {data: { todo: todo }}
+    );
+    loadTodos();
+  };
+  const addTodo = async () => {
+    const response = await axios.post(`${Base_URL}/todos`,{todo: newTodo});
+
+    setNewTodo("");
+    loadTodos();
+  };
+
+  const editTodo = async () => {
+    const response = await axios.put(`${Base_URL}/todos`,
+      {oldTodo: oldTodo, newTodo: newTodo}
+    );
+    setNewTodo("");
+    setOldTodo("");
+    setIsUpdating(false);
+    loadTodos();
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  return (
+    <div >
+      <h1>Todo List</h1>
+      <div className='input-box'>
+          <input type="text" 
+          placeholder='Enter a todo' 
+          className='input'
+          value={newTodo} 
+          onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button className='input-btn'
+          onClick={() => {
+            if(isUpdating){
+              editTodo();
+            }else{
+              addTodo();
+            }
+          }}
+          >{isUpdating ? "Update" : "Add"}</button>
+        </div>
+      <div className='todo-container'>
+        {todos.map((todo,index) => {
+          return (
+            <div key={index} className='todo-card'>
+                <p>{todo}</p>
+                <div className='btn-container'>
+                  <img src={UpdateImg}
+                  alt="update"
+                  className="update-btn"
+                  onClick={()=>
+                  {
+                    setIsUpdating(true);
+                    setOldTodo(todo);
+                    setNewTodo(todo);
+                  }
+                  }
+                  />
+
+                  <img src={DeleteImg}
+                  alt="delete"
+                  className="delete-btn"
+                  onClick={() => deleteTodo(todo)}/>
+                </div>
+
+            </div>);}
+        )}
+      </div>
+       
+    </div>
+  );
+}
+
+export default App
